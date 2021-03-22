32a335fe959cf5997bef13b29bff7f9b1f4a39ab
Manu024 <manumadhu2906@gmail.com>
2021-03-22 13:56:21 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+// Set your secret key. Remember to switch to your live secret key in production!
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+require("dotenv").config();
+const express = require("express");
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SK);
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+
+app.get("/api/payment", (req, res) => {
+  res.send("Server Started Its Process");
+});
+
+app.post("/api/payment", async (req, res) => {
+  const { id, amount } = req.body;
+  console.log(req.body);
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "INR",
+      payment_method: id,
+      confirm: true,
+      description: "test payment",
+    });
+    return res.status(200).json({
+      message: payment.status,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(port, () => console.log("[Node Server Started]"));
